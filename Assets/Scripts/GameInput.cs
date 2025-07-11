4abec5cf64e9b2ab8fc6df6240583542f4e46294
Trevor <trevorjloth@gmail.com>
2025-07-11 17:07:26 -0400

    Refactoring input system
    - Added new input system to project
    - Removed old system
    - Added arrow key controls alongside WASD controls

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>(); // Get 2D vector from inputs
+        
+        // Normalization can be done through processors within the InputActions Asset
+        inputVector = inputVector.normalized; // normalize the vector to ensure consistent movement speed in all directions
+
+        return inputVector; // return a normalized vector reflecting the movement direction
+    }
+}
