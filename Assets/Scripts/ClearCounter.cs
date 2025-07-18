d4c973f17a291cd4776a1c1f8c321a9b6ce238c0
Trevor <trevorjloth@gmail.com>
2025-07-18 00:45:26 -0400

    Made Scriptable Objects for some Kitchen Foods
    - Made KitchenObjectSO
    -- Stores the prefab, sprite, and name
    - Made KitchenObject Class as a means of getting the SO

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact()
     {
         Debug.Log("Interact!");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint); // Instantiate a tomato prefab at the counter top point
+        kitchenObjectTransform.localPosition = Vector3.zero; // Set the local position to zero so it appears at the counter top
     }
 }
